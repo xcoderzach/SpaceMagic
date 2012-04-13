dfa24d7d9d6ebb5c8e2f01c40072bb47ba88b30f
Eugene Butler <eugene.iv@gmail.com>
2012-04-12 22:52:59 -0500

    added first tutorial

--- tutorials/tutorialsmaker.sh
+++ tutorials/tutorialsmaker.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+echo '
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>SpaceMagic - About</title>
+    <title>SpaceMagic</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
     <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/style.css" rel="stylesheet">
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
@@ -18,14 +20,29 @@
   <body>
     <div class="container">
       <header>
-        <h1 class="superbig">About</h1>
-        <h2>Origin Story</h2>
-        <p>Last year, we (re)started work on a realtime web app but found ourselves stymied by the limitations of Ruby on Rails. We decided to bite the bullet, move to Node, and build our app on a cool, real-time framework. Since none existed, we built our own. Every time we needed support for a new feature, we implemented it through the framework. SpaceMagic is the ongoing result of this process. It's a realtime web framework shaped by the requirements of a real, large web app project.</p>
-      <hr>
-      <footer>
-        <p><a href="http://eugene4.com">Eugene Butler</a> (eugene@eugene4.com) and <a href="http://github.com/xcoderzach">Zach Smith</a> (x.coder.zach@gmail.com).</p>
-      </footer>
+        <h1 class="superbig">SPACE MAGIC</h1>
+        <h2>Simple Tutorials</h2>
+      </header>
+      <div class="row links">
+        <div class="span3"><h3><img src="assets/img/icon-github.png"> <a href="http://github.com/xcoderzach/spacemagic">Visit our GitHub Repo</a></h3></div>
+        <div class="span3 center"><h3><a href="tutorials/">View Tutorials</a></h3></div>
+        <div class="span3 center"><h3><a href="docs/">Read API Docs</a></h3></div>
+        <div class="span3 center"><h3><a href="about.html">About SpaceMagic</a></h3></div>
+      </div>
+      <hr class="linksHr">
+      <div class="row">
+      <div class="span6">'>index.html
 
+curl https://raw.github.com/xcoderzach/SpaceMagic/master/docs/todo.markdown > allthedocs.md
+marked --gfm allthedocs.md >> index.html
+echo '
+          </div>
+          <div class="span5 offset1">
+            <iframe src="http://165.225.128.119" frameborder="0" height="600px" width="415px" scrolling="no"></iframe>
+          </div>
+        </div>
+        </div>
+      </div>
     </div> <!-- /container -->
     <script src="http://twitter.github.com/bootstrap/assets/js/jquery.js"></script>
     <script src="http://twitter.github.com/bootstrap/assets/js/bootstrap-transition.js"></script>
@@ -41,4 +58,5 @@
     <script src="http://twitter.github.com/bootstrap/assets/js/bootstrap-carousel.js"></script>
     <script src="http://twitter.github.com/bootstrap/assets/js/bootstrap-typeahead.js"></script>
   </body>
-</html>
+</html>' >> index.html
+rm allthedocs.md
